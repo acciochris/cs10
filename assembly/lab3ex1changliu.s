84ddb413d5efceb4391ff16bdf33fc7d6d13238b
Chris Liu <59787860+acciochris@users.noreply.github.com>
2023-10-09 22:41:47 +0000

    Add boilerplate

--- assembly/lab3ex1changliu.s
+++ assembly/lab3ex1changliu.s
@@ -19,19 +19,104 @@
 # | read integer  | 5   | $v0: integer                 |
 
 .data
+.align 2
+M: .space 64
+input_vector: .space 13 # 2*4 (ints) + 1*3 (spaces) + 1 (\0) + 1 (extra buffer)
+prompt_row: .asciiz "Row "
+prompt_row2: .asciiz ": "
+result_sum: .asciiz "Sum: "
+result_parity_0: .asciiz "Parity: 0\n"
+result_parity_1: .asciiz "Parity: 1\n"
+newline: .asciiz "\n"
 
 .text
 .globl main
 main:
+    li $s0, 4
+    la $s2, M
+    main_read_matrix:
+        la $a0, prompt_row
+        jal _print_str
+
+        # print row index
+        li $s1, 5
+        sub $s1, $s1, $s0
+        move $a0, $s1
+        jal _print_int
+
+        la $a0, prompt_row2
+        jal _print_str
+
+        # input
+        la $a0, input_vector
+        li $a1, 13
+        li $v0, 8
+        syscall
+
+        # calculate array offset
+        addiu $t0, $s1, -1 # row 1 => offset 0
+        sll $t0, $t0, 4    # 4 words = 16 bytes = 2^4
+        addu $a1, $s2, $t0
+
+        jal parse_vector
+
+        addiu $s0, $s0, -1
+        bne $s0, $zero, main_read_matrix
+
+    # sum
+    li $s0, 16
+    li $s1, 0
+    main_sum:
+        addiu $s0, $s0, -1
+        sll $t0, $s0, 2
+        addu $t0, $s2, $t0
+        lw $t0, 0($t0)
+        addu $s1, $s1, $t0
+        bne $s0, $zero, main_sum
+
+    la $a0, result_sum
+    jal _print_str
+
+    move $a0, $s1
+    jal _print_int
+
+    la $a0, newline
+    jal _print_str
+
+    # parity with if-then else
+    andi $t0, $s1, 0x1
+    bne $t0, $zero, main_parity_1
+    la $a0, result_parity_0
+    jal _print_str
+    j main_end_parity
+    main_parity_1:
+        la $a0, result_parity_1
+        jal _print_str
+    main_end_parity:
+
     # exit
     li $v0, 10
     syscall
 
 # parameters:
+# $a0: integer to print
+_print_int:
+    li $v0, 1
+    syscall
+    jr $ra
+
+# parameters:
+# $a0: null-terminated string to print
+_print_str:
+    li $v0, 4
+    syscall
+    jr $ra
+
+# parameters:
 # $a0: (char *) pointer to a string of four space-separated 2-digit integers
 # $a1: (int *) pointer to where the integers should be stored
 parse_vector:
-    addi $sp, $sp, -16
+    addiu $sp, $sp, -16
     sw $ra, 0($sp)
     sw $s0, 4($sp)
     sw $s1, 8($sp)
@@ -45,16 +130,16 @@ parse_vector:
         move $a0, $s0
         jal atoi
         sw $v0, 0($s1)
-        addi $s0, $s0, 3
-        addi $s1, $s1, 4
-        addi $s2, $s2, -1
+        addiu $s0, $s0, 3
+        addiu $s1, $s1, 4
+        addiu $s2, $s2, -1
         bne $s2, $zero, parse_vector_loop
 
     lw $s2, 12($sp)
     lw $s1, 8($sp)
     lw $s0, 4($sp)
     lw $ra, 0($sp)
-    addi $sp, $sp, 16
+    addiu $sp, $sp, 16
     jr $ra
 
 # parameters:
@@ -70,11 +155,11 @@ atoi:
     sll $t0, $t0, 1 # 2X
     move $v0, $t0
     sll $t0, $t0, 2 # 2X * 4 = 8X
-    add $v0, $v0, $t0
+    addu $v0, $v0, $t0
 
     # lower digit
     lbu $t0, 1($a0)
     andi $t0, $t0, 0xF
-    add $v0, $v0, $t0
+    addu $v0, $v0, $t0
 
     jr $ra
