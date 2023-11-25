081807383d124ba207269bd5d4da8a6d1174c0e5
Chris Liu <59787860+acciochris@users.noreply.github.com>
2023-11-25 19:10:38 +0000

    Import from exercise 1

--- assembly/lab6ex2changliu.s
+++ assembly/lab6ex2changliu.s
@@ -20,10 +20,26 @@
 
 .data
 .align 2
-M1: .space 64
-M2: .space 64
-M3: .space 64
-input_vector: .space 9 # 1*4 (ints) + 1*3 (spaces) + 1 (\0) + 1 (extra buffer)
+M1:  # identity matrix
+.word 1, 0, 0, 0, 0, 0, 0, 0
+.word 0, 1, 0, 0, 0, 0, 0, 0
+.word 0, 0, 1, 0, 0, 0, 0, 0
+.word 0, 0, 0, 1, 0, 0, 0, 0
+.word 0, 0, 0, 0, 1, 0, 0, 0
+.word 0, 0, 0, 0, 0, 1, 0, 0
+.word 0, 0, 0, 0, 0, 0, 1, 0
+.word 0, 0, 0, 0, 0, 0, 0, 1
+M2:  # some random matrix
+.word 1, 5, 4, 7, 2, 4, 8, 6
+.word 9, 3, 8, 4, 6, 2, 6, 5
+.word 0, 1, 2, 6, 5, 8, 5, 4
+.word 8, 3, 6, 8, 3, 8, 6, 6
+.word 8, 7, 7, 9, 3, 4, 6, 1
+.word 8, 5, 6, 3, 5, 6, 0, 1
+.word 1, 9, 8, 2, 5, 6, 8, 0
+.word 0, 1, 2, 6, 5, 7, 2, 9
+M3:
+.space 256
 prompt_row: .asciiz "Row "
 prompt_row2: .asciiz ": "
 newline: .asciiz "\n"
@@ -32,16 +48,6 @@ space: .asciiz " "
 .text
 .globl main
 main:
-    # read in two matrices
-    la $a0, M1
-    jal read_matrix
-
-    la $a0, newline
-    jal _print_str
-
-    la $a0, M2
-    jal read_matrix
-
     # multiply
     la $a0, M3
     la $a1, M1
@@ -74,55 +80,6 @@ _print_str:
     jr $ra
 
 # parameters:
-# $a0: (int *) pointer to where the integers should be stored
-read_matrix:
-    addiu $sp, $sp, -16
-    sw $ra, 0($sp)
-    sw $s0, 4($sp)
-    sw $s1, 8($sp)
-    sw $s2, 12($sp)
-
-    li $s0, 4
-    move $s2, $a0
-
-    read_matrix_loop:
-        la $a0, prompt_row
-        jal _print_str
-
-        # print row index
-        li $s1, 5
-        sub $s1, $s1, $s0
-        move $a0, $s1
-        jal _print_int
-
-        la $a0, prompt_row2
-        jal _print_str
-
-        # input
-        la $a0, input_vector
-        li $a1, 9
-        li $v0, 8
-        syscall
-
-        # calculate array offset
-        addiu $t0, $s1, -1 # row 1 => offset 0
-        sll $t0, $t0, 4    # 4 words = 16 bytes = 2^4
-        addu $a1, $s2, $t0
-
-        jal parse_vector
-
-        addiu $s0, $s0, -1
-        bne $s0, $zero, read_matrix_loop
-
-    lw $s2, 12($sp)
-    lw $s1, 8($sp)
-    lw $s0, 4($sp)
-    lw $ra, 0($sp)
-    addiu $sp, $sp, 16
-
-    jr $ra
-
-# parameters:
 # $a0: (int *) pointer to matrix
 print_matrix:
     addiu $sp, $sp, -24
@@ -133,7 +90,7 @@ print_matrix:
     sw $s3, 16($sp)
     sw $s4, 20($sp)
 
-    li $s0, 4
+    li $s0, 8
     move $s2, $a0
 
     print_matrix_loop:
@@ -141,7 +98,7 @@ print_matrix:
         jal _print_str
 
         # print row index
-        li $s1, 5
+        li $s1, 9
         sub $s1, $s1, $s0
         move $a0, $s1
         jal _print_int
@@ -151,7 +108,7 @@ print_matrix:
 
         # calculate array offset
         addiu $t0, $s1, -1 # row 1 => offset 0
-        sll $t0, $t0, 4    # 4 words = 16 bytes = 2^4
+        sll $t0, $t0, 5    # 8 words = 32 bytes = 2^5
         addu $s3, $s2, $t0
 
         # print first integer
@@ -159,7 +116,7 @@ print_matrix:
         jal _print_int
 
         # print rest with spaces
-        li $s4, 3
+        li $s4, 7
         print_matrix_inner_loop:
             la $a0, space
             jal _print_str
@@ -199,9 +156,9 @@ multiply_matrix:
     sw $s2, 8($sp)
 
     # pseudo-code
-    # for (int i = 0; i < 4; ++i) {
-    #   for (int j = 0; j < 4; ++j) {
-    #     for (int k = 0; k < 4; ++k) {
+    # for (int i = 0; i < 8; ++i) {
+    #   for (int j = 0; j < 8; ++j) {
+    #     for (int k = 0; k < 8; ++k) {
     #       $a0[i][j] += $a1[i][k] * $a2[k][j];
     #     }
     #   }
@@ -210,30 +167,30 @@ multiply_matrix:
     move $s1, $a1
     move $s2, $a2
 
-    li $t3, 4
+    li $t3, 8
 
     multiply_matrix_loop1:
         addiu $t3, $t3, -1
-        li $t4, 4
+        li $t4, 8
 
         multiply_matrix_loop2:
             addiu $t4, $t4, -1
-            li $t5, 4
+            li $t5, 8
 
             multiply_matrix_loop3:
                 addiu $t5, $t5, -1
 
-                # one row: 4 * 4 = 16 bytes
+                # one row: 4 * 8 = 32 bytes
                 # one element: 4 bytes
 
                 # load
-                sll $t6, $t3, 4
+                sll $t6, $t3, 5
                 sll $t7, $t5, 2
                 addu $t1, $t6, $t7
                 addu $t1, $t1, $s1
                 lw $t1, 0($t1)
 
-                sll $t6, $t5, 4
+                sll $t6, $t5, 5
                 sll $t7, $t4, 2
                 addu $t2, $t6, $t7
                 addu $t2, $t2, $s2
@@ -243,7 +200,7 @@ multiply_matrix:
                 mul $t1, $t1, $t2
 
                 # add in place
-                sll $t6, $t3, 4
+                sll $t6, $t3, 5
                 sll $t7, $t4, 2
                 addu $t0, $t6, $t7
                 addu $t0, $t0, $s0
@@ -262,47 +219,5 @@ multiply_matrix:
     lw $s1, 4($sp)
     lw $s0, 0($sp)
     addiu $sp, $sp, 12
-
-    jr $ra
-
-# parameters:
-# $a0: (char *) pointer to a string of four space-separated digits
-# $a1: (int *) pointer to where the integers should be stored
-parse_vector:
-    addiu $sp, $sp, -16
-    sw $ra, 0($sp)
-    sw $s0, 4($sp)
-    sw $s1, 8($sp)
-    sw $s2, 12($sp)
-
-    move $s0, $a0
-    move $s1, $a1
-    li $s2, 4
-
-    parse_vector_loop:
-        move $a0, $s0
-        jal atoi
-        sw $v0, 0($s1)
-        addiu $s0, $s0, 2
-        addiu $s1, $s1, 4
-        addiu $s2, $s2, -1
-        bne $s2, $zero, parse_vector_loop
-
-    lw $s2, 12($sp)
-    lw $s1, 8($sp)
-    lw $s0, 4($sp)
-    lw $ra, 0($sp)
-    addiu $sp, $sp, 16
-
-    jr $ra
-
-# parameters:
-# $a0: (char *) pointer to a character representing a digit
-# returns:
-# $v0: (int) parsed integer
-atoi:
-    # upper digit
-    lbu $v0, 0($a0)
-    addiu $v0, $v0, -48
 
     jr $ra
